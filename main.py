457042837f5b157e138a3ec58dc0795dd3eb0543
Emeka Okonkwo <chukwuemekaokonkwo24@gmail.com>
2026-04-14 10:28:04 +0100

    Find your BMI

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # TODO 1: Replace the below code to accept the "weight" and "height" values as input from the user
-weight = 1
-height = 1
+weight = float(input("What is you weight in Kilograms? "))
+height = float(input("What is yur height in Meters? "))
 
 # TODO 2: Calculate the BMI value based on the weight and height values
-BMI = 5
+height = height ** 2
+
+BMI = weight / height
 
 # This is a handy function to round the number to a 1 decimal point
 # Don't change the code below
@@ -15,5 +17,5 @@ print("\n------BMI Categories------")
 
 print("<18.5        : Underweight")
 print("18.5 – 24.9  : Normal weight")
-print("25 - 29.9    : Underweight")
+print("25 - 29.9    : Overweight")
 print(">=30         : Obesity")
